d5b538bdef0395b0338217663630e3304f8fc4bd
Guilherme Costa <114708311+Zodklin@users.noreply.github.com>
2024-07-23 11:44:39 -0300

    Sql mysql 11 07 (#22)
    * ex. sql
    
    * correcao ex. sql
    
    * Entrega exercicios sql 11-07

---
+++ SQL-MYSQL/exercicio-filme/dados.sql
@@ -0,0 +1,24 @@
+-- Instrução dos filmes sugeridos:
+
+INSERT INTO 
+	filmes
+		(titulo, titulo_original, ano_lancamento, classificacao, duracao, genero, avaliacao)
+VALUES 
+	('Duna: Parte 2', 'Dune: Part Two', 2024, 14, 166, 'Ação, Aventura, Drama', '8.6'),
+    ('Divertida mente', 'Inside out', 2015, 0 , 95, 'Animação, Aventura, Comédia', '8.1');
+
+-- Instrução dos filmes no CSV
+
+INSERT INTO 
+	filmes
+		(titulo, titulo_original, ano_lancamento, classificacao, duracao, genero, avaliacao)
+VALUES
+	('O Senhor dos Anéis: A Sociedade do Anel', 'The Lord of The Rings: The Fellowship of the Ring', 2001, 12, 178, 'Aventura, Fantasia', '8.8'),
+    ('O Poderoso Chefão', 'The Godfather', 1972, 16, 175, 'Crime, Drama', '9.2'),
+    ('Interestelar', 'Interstellar', 2014, 10, 169, 'Aventura, Drama, Sci-Fi', '8.6'),
+    ('Clube da Luta', 'Fight Club', 1999, 18, 139, 'Drama', '8.8'),
+    ('Matrix', 'The Matrix', 1999, 16, 136, 'Ação Sci-Fi', '8.7'),
+    ('Cidade de Deus', 'City of God', 2002, 18, 130, 'Crime, Drama', '8.6'),
+    ('Parque dos Dinossauros', 'Jurassic Park', 1993, 0, 127, 'Aventura, Sci-Fi' , '8.1'),
+    ('O Iluminado', 'The Shining', 1980, 16, 146, 'Drama, Horror', '8.4'),
+    ('A Origem', 'Inception', 2010, 14, 148, 'Sci-Fi, Thriller', '8.8');
